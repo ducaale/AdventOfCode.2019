e6f82a888821b25a3c7c02624c5a8e0ed6bfc525
ducaale <sharaf.13@hotmail.com>
2019-12-08 01:19:08 +0000

    solve part2 of day3 and refactor it

--- Day03.fsx
+++ Day03.fsx
@@ -1,45 +1,45 @@
 open System.IO
 
 [<StructuredFormatDisplay("({x},{y})")>]
-type Coordinate = { x: int; y: int }
+type Point = { x: int; y: int }
 
-type Line = Coordinate * Coordinate
+type Line = Point * Point
 
 type LineType = | Horizontal | Vertical
 
-type Movement =
+type Stride =
     | Up of int
     | Down of int
     | Right of int
     | Left of int
 
 let input =
-    let commandToMovement (command: string) =
-        let movement = int command.[1..]
-        match command.[0] with
-        | 'U' -> Up(movement)
-        | 'D' -> Down(movement)
-        | 'R' -> Right(movement)
-        | 'L' -> Left(movement)
-        | c -> failwithf "unknown command %c" c
+    let parseStride (text: string) =
+        let steps = int text.[1..]
+        match text.[0] with
+        | 'U' -> Up(steps)
+        | 'D' -> Down(steps)
+        | 'R' -> Right(steps)
+        | 'L' -> Left(steps)
+        | c -> failwithf "unknown direction %c" c
 
     let lines =
         @"./InputFiles/day03.txt"
         |> File.ReadAllLines
         |> Array.map (fun l ->
             l.Split(',')
-            |> Array.map commandToMovement
+            |> Array.map parseStride
             |> List.ofArray)
 
-    (lines.[0], lines.[1])
-
-let lineType line =
-    let (s, e) = line
-    if s.x = e.x then Vertical
-    else Horizontal
+    lines.[0], lines.[1]
 
 // intersection of two perpendicular lines
 let intersection line1 line2 =
+    let lineType line =
+        let s, e = line
+        if s.x = e.x then Vertical
+        else Horizontal
+
     let isBetween n x y = (n >= x && n <= y) || (n >= y && n <= x)
     let x ({ x = x }, _) = x
     let y ({ y = y }, _) = y
@@ -54,45 +54,45 @@ let intersection line1 line2 =
         elif not (isBetween (x line2) (x1 line1) (x2 line1)) then None
         else Some { x = (x line2); y = (y line1) }
 
-    match ((lineType line1), (lineType line2)) with
+    match (lineType line1), (lineType line2) with
     | (Horizontal, Vertical) -> intersection' line1 line2
     | (Vertical, Horizontal) -> intersection' line2 line1
     | _ -> None
 
-let manhattanFromOrigin pos = (abs pos.x) + (abs pos.y)
+let manhattan pos1 pos2 =
+    abs (pos1.x - pos2.x) + abs (pos1.y - pos2.y)
 
-let advance position movement =
-    match movement with
-    | Up steps -> { position with y = position.y + steps }
-    | Down steps -> { position with y = position.y - steps }
-    | Right steps -> { position with x = position.x + steps }
-    | Left steps -> { position with x = position.x - steps }
+let length line =
+    let s, e = line
+    if s.x = e.x then abs (s.y - e.y)
+    else abs (s.x - e.x)
 
-let rec pathToCoordinates coordinates path =
-    if List.isEmpty coordinates then failwith "coordinates shouldn't be empty"
-    if List.isEmpty path then
-        coordinates
-    else
-        let pos = List.head coordinates
-        let coordinates = (advance pos (List.head path)) :: coordinates
-        pathToCoordinates coordinates (List.tail path)
+let moveByStride pos stride =
+    match stride with
+    | Up steps -> { pos with y = pos.y + steps }
+    | Down steps -> { pos with y = pos.y - steps }
+    | Right steps -> { pos with x = pos.x + steps }
+    | Left steps -> { pos with x = pos.x - steps }
 
-let rec coordinatesToLines lines coordinates =
-    if (List.length coordinates) < 2 then
-        lines
-    else
-        let line = (coordinates.[1], coordinates.[0])
-        let lines = line :: lines
-        coordinatesToLines lines (List.tail coordinates)
+let stridesToPoints =
+    List.scan moveByStride { x = 0; y = 0}
 
-let lines path =
-    let initialPos = { x = 0; y = 0 }
-    path |> pathToCoordinates [ initialPos ] |> coordinatesToLines []
+let rec pointsToLines =
+    List.pairwise
+
+let linesToCumulativeSteps =
+    List.scan (fun total line -> total + length line) 0
+    >> List.rev
+    >> List.tail
+    >> List.rev
+
+let stridesToLines =
+    stridesToPoints >> pointsToLines
 
 let part1() =
-    let path1, path2 = input
-    let wire1 = lines path1
-    let wire2 = lines path2
+    let strides1, strides2 = input
+    let wire1 = stridesToLines strides1
+    let wire2 = stridesToLines strides2
 
     let intersections =
         seq {
@@ -100,7 +100,30 @@ let part1() =
                 for line2 in wire2 do
                     let intersection = intersection line1 line2
                     if intersection.IsSome then intersection.Value
-        }
+        } |> Seq.tail
 
-    // assume that intersection at (0,0) occurs only once
-    intersections |> Seq.tail |> Seq.map manhattanFromOrigin |> Seq.min
+    intersections
+    |> Seq.map (manhattan { x = 0; y = 0 })
+    |> Seq.min
+
+let part2() =
+    let strides1, strides2 = input
+
+    let wire1 = stridesToLines strides1
+    let wire2 = stridesToLines strides2
+
+    let signalDelays1 = linesToCumulativeSteps wire1
+    let signalDelays2 = linesToCumulativeSteps wire2
+
+    let signalDelayAtIntersections =
+        seq {
+            for line1, delay1 in List.zip wire1 signalDelays1 do
+                for line2, delay2 in List.zip wire2 signalDelays2 do
+                    let intersection = intersection line1 line2
+                    let s1, _ = line1
+                    let s2, _ = line2
+                    if intersection.IsSome then
+                        delay1 + (length (s1, intersection.Value)) + delay2 + (length (s2, intersection.Value))
+        } |> Seq.tail
+
+    signalDelayAtIntersections |> Seq.min
